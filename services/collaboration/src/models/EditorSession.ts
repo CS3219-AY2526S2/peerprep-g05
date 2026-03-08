a84feaa883dadea1c62f826a542f05f22b8a1036
PohAnson <50758680+pohanson@users.noreply.github.com>
2026-03-08 21:36:22 +0800

    Init mongoose
    * Add `editorSessionSchema`

---
+++ services/collaboration/src/models/EditorSession.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const editorSessionSchema = new Schema({
+  roomStatus: {
+    type: String,
+    enum: [
+      "ACTIVE", // As long as one of the 2 users is still in the session.
+      "ENDED", // Initiated by any user, applies to both users.
+    ],
+  },
+  descriptionContent: String,
+  editorContent: String,
+  userId: {
+    type: [String],
+    length: 2,
+    reason: "Needs exactly 2 users in a session",
+  },
+  startedOn: {
+    type: Date,
+    default: Date.now,
+  },
+  endedOn: Date,
+});
+
+export const EditorSessionModel = model("EditorSession", editorSessionSchema);
--- services/collaboration/src/server.ts
+++ services/collaboration/src/server.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/peerprep-collaboration");
+
 const app = express();
 
 const router = express.Router();
