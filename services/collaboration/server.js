13f2eb80acd83adae086ba61411f38d064db5d58
PohAnson <50758680+pohanson@users.noreply.github.com>
2026-03-08 12:06:10 +0800

    Init Collaboration microservice

---
+++ services/collaboration/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+const app = express();
+
+const router = express.Router()
+
+router.get("/", (req, res) => res.send("Hi"))
+
+app.use("/api/v1", router)
+
+app.listen(3000, (e) => {
+    if (e) {
+        console.error(e)
+    }
+    console.log("Server is running...");
+});
