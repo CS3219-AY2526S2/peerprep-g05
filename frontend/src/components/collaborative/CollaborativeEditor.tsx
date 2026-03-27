cd840952902f82c8890b5c80d4b24e8d2ba796b2
PohAnson <50758680+pohanson@users.noreply.github.com>
2026-03-27 15:49:39 +0800

    Add split pane for question

---
+++ frontend/src/components/collaborative/CollaborativeEditor.tsx
@@ -0,0 +1,64 @@
+import { EditorState } from "@codemirror/state";
+import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { yCollab } from "y-codemirror.next";
+import { WebsocketProvider } from "y-websocket";
+import * as Y from "yjs";
+
+const WEBSOCKET_URL = import.meta.env.VITE_WEBSOCKET_URL;
+if (!WEBSOCKET_URL) {
+  throw new Error(
+    "VITE_WEBSOCKET_URL is not set. Define it in the frontend .env file.",
+  );
+}
+
+const editorTheme = EditorView.theme({
+  "&": {
+    height: "100%",
+  },
+});
+
+export function CollaborativeEditor({ roomId }: { roomId: string }) {
+  const editorContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const ydoc = new Y.Doc();
+    const provider = new WebsocketProvider(WEBSOCKET_URL, roomId, ydoc);
+    provider;
+
+    const ytext = ydoc.getText();
+    provider.awareness.setLocalStateField("user", {
+      name: "User1",
+      color: "#ccc",
+    });
+
+    const state = EditorState.create({
+      doc: ytext.toString(),
+      extensions: [basicSetup, editorTheme, yCollab(ytext, provider.awareness)],
+    });
+
+    const view = new EditorView({
+      state: state,
+      parent: editorContainerRef.current!,
+    });
+
+    return () => {
+      view.destroy();
+      ydoc.destroy();
+    };
+  }, [roomId]);
+
+  return (
+    <div
+      id="editor"
+      ref={editorContainerRef}
+      style={{
+        height: "100%",
+        minHeight: "400px",
+        border: "1px solid black",
+      }}
+    />
+  );
+}
--- frontend/src/pages/CollaborativeEditor.tsx
+++ frontend/src/pages/CollaborativeEditor.tsx
@@ -1,69 +1,19 @@
-import { EditorState } from "@codemirror/state";
-import { EditorView } from "@codemirror/view";
-import { basicSetup } from "codemirror";
-import { useEffect, useRef } from "react";
+import { CollaborativeEditor } from "../components/collaborative/CollaborativeEditor";
+import { QuestionDescriptionPreview } from "../components/QuestionDescriptionPreview";
 import { useParams } from "react-router-dom";
-import { yCollab } from "y-codemirror.next";
-import { WebsocketProvider } from "y-websocket";
-import * as Y from "yjs";
 
-const WEBSOCKET_URL = import.meta.env.VITE_WEBSOCKET_URL;
-if (!WEBSOCKET_URL) {
-  throw new Error(
-    "VITE_WEBSOCKET_URL is not set. Define it in the frontend .env file.",
-  );
-}
-
-const editorTheme = EditorView.theme({
-  "&": {
-    height: "100%",
-  },
-});
-
-export default function CollaborativeEditor() {
-  const editorContainerRef = useRef<HTMLDivElement>(null);
-
+export default function CollaborativeEditorPage() {
   const { roomId } = useParams();
   if (!roomId) {
     return <div>Room ID is required</div>;
   }
-
-  useEffect(() => {
-    const ydoc = new Y.Doc();
-    const provider = new WebsocketProvider(WEBSOCKET_URL, roomId, ydoc);
-    provider;
-
-    const ytext = ydoc.getText();
-    provider.awareness.setLocalStateField("user", {
-      name: "User1",
-      color: "#ccc",
-    });
-
-    const state = EditorState.create({
-      doc: ytext.toString(),
-      extensions: [basicSetup, editorTheme, yCollab(ytext, provider.awareness)],
-    });
-
-    const view = new EditorView({
-      state: state,
-      parent: editorContainerRef.current!,
-    });
-
-    return () => {
-      view.destroy();
-      ydoc.destroy();
-    };
-  }, [roomId]);
-
   return (
-    <div
-      id="editor"
-      ref={editorContainerRef}
-      style={{
-        height: "100%",
-        minHeight: "400px",
-        border: "1px solid black",
-      }}
-    />
+    <div className="h-full grid grid-cols-2">
+      <QuestionDescriptionPreview
+        description={`# Question Title
+This is a **Question description**. Supports markdown`}
+      />
+      <CollaborativeEditor roomId={roomId} />
+    </div>
   );
 }
