97d6eef62d6d8b5db58d05777cf7e68c9a5f4280
seanrh34 <seanhardjanto034@gmail.com>
2026-04-16 00:48:23 +0800

    Fix gateway rate limiting strictness

--- services/user/server.js
+++ services/user/server.js
@@ -11,6 +11,7 @@ import config from "./config/index.js";
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(cors({
     origin: config.frontendUrl,
     credentials: true,
